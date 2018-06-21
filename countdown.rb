75b96e8b301d407a3aca6b0035df061a6984d6d1
William Chabla <w_chabla@hotmail.com>
2018-06-21 06:39:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  until time = 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    
+  end
+  "HAPPY NEW YEAR!"
+end
